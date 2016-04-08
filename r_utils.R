1611c649fa1d41f0244e82833977204614a9510b
Shom Mazumder <shom.mazumder@gmail.com>
2016-04-08 13:23:08 -0400

    upload init script

---
+++ r_utils.R
@@ -0,0 +1,20 @@
+#packages
+library(reshape)
+library(dplyr)
+library(ggplot2)
+
+####COLORS####
+night <- rgb(26,31,30, max=255)
+beach <- rgb(227,223,186, max=255)
+red60 <- rgb(1,.4,.4)
+tangyblue <- rgb(108,189,181, max=255)
+purp <- rgb(181.5,145.5, 141.5, max=255)
+
+
+green1 <- rgb(178,179,159, max = 255)
+lightbrown <- rgb(205,140,82, max = 255)
+
+green2 <- rgb(200,201,181, max = 255)
+green3 <- rgb(222,223,197, max = 255)
+orange <- rgb(61,66,60, max = 255)
+aqua <- rgb(240,236,201, max = 255)
