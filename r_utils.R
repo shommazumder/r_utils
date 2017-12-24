9e41083c6ad572ac8b9a1a5b65d752b0a18cbf03
Shom Mazumder <shom.mazumder@gmail.com>
2017-12-23 21:52:32 -0500

    added map theme

--- r_utils.R
+++ r_utils.R
@@ -28,6 +28,43 @@ olive <- rgb(164,199,6,max = 255)
 orangejuice <- rgb(252,135,16,max = 255)
 flymetothemaroon <- rgb(159,25,22,max = 255)
 
+#### GGPLOT THEMES ####
+
+theme_shom <- function(){
+  theme_minimal() +
+  theme(
+    text = element_text(family = "Minion Pro", color = "#22211d",size = 12),
+    #axis.text.x = element_text(angle = 45, hjust = 1),
+    # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+    panel.grid.major = element_line(color = "#ebebe5", size = 0.5),
+    panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+    plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+    panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    legend.position = "none",
+    panel.border = element_blank())
+}
+
+theme_map <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(color = "#22211d"),
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+      panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+      legend.background = element_rect(fill = "#f5f5f2", color = NA),
+      panel.border = element_blank()
+    )
+}
+
 ####DATA PREP####
 
 stata_codebook <- function(x) {
